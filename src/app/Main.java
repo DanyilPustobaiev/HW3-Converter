4da335a624c4235593057cf4f81961e0c1eb60f4
DanyilPustobaiev <11pustobaev.d@gmail.com>
2024-06-11 22:05:36 +0200

    Add conversion method Fahrenheit to Celsius

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,17 @@
 package app;
 
 public class Main {
+
+    private static final double CONV_T = 32;
+
     public static void main(String[] args) {
         System.out.println("Temperature units converter");
+
+        double degreeFrt = 1;
+        double degreeCelsius = convFrtToCls(degreeFrt);
+        System.out.println("From Fahrenheit to Celsius. Result is: " + degreeCelsius + " Celsius degree");
+    }
+    private static double convFrtToCls(double degreeFrt){
+        return (degreeFrt - CONV_T) * 5/9;
     }
 }
